e6ebf2c5b44348d23b6b8697d956ee29068f282d
Florencia Cabrera <aflorenciacabrera@gmail.com>
2019-06-06 23:33:17 -0300

    if home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 class HomeController extends Controller
 {
     /**
@@ -21,10 +21,15 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Request $request)
     {
-        $request->user()->authorizeRoles(['user', 'admin']);
-        return view('home');
+        if (Auth::user()->hasRole('admin')) {
+            return view('home');  
+        }
+        else {
+            return view('home');  
+        }
+        // return view('home');
     }
 
     /*
