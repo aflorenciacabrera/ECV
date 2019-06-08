f04e5e296de2a1c12190244876df5624614f92ab
Florencia Cabrera <aflorenciacabrera@gmail.com>
2019-06-08 10:04:15 -0300

    encuesta

--- app/Http/Controllers/EncuestaController.php
+++ app/Http/Controllers/EncuestaController.php
@@ -7,5 +7,14 @@ use Illuminate\Http\Request;
 class EncuestaController extends Controller
 {
     //
+    public function crearEncuesta (Request $request)
+      {
+        $e = new encuesta; 
+
+        $e->name= $request->name;
     
+        $e->save();
+     // return view("institucion.mostrarCapacidad");
+      return redirect(url('inicio'))->with('status','El usuario cliente fue registrado');;
+      }
 }
