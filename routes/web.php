115af9a3f1cdf87f8588f794c7f905361cee4885
aflorenciacabrera <aflorenciacabrera@gmail.com>
2019-06-08 11:54:45 -0300

    encuesta

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,16 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
+
+// Encuestas
+Route::get('encuesta', 'EncuestaController@verEncuesta')->name('encuesta');
+Route::post('encuesta/crear', 'EncuestaController@crearEncuesta')->name('crearencuesta');
+
+Route::get('encuesta/seccion_1', 'EncuestaController@verseccion1')->name('seccion1');
+Route::get('encuesta/seccion_2', 'EncuestaController@verseccion2')->name('seccion2');
+Route::get('encuesta/seccion_3', 'EncuestaController@verseccion3')->name('seccion3');
+Route::get('encuesta/seccion_4', 'EncuestaController@verseccion4')->name('seccion4');
+Route::get('encuesta/seccion_5', 'EncuestaController@verseccion5')->name('seccion5');
+Route::get('encuesta/seccion_6', 'EncuestaController@verseccion6')->name('seccion6');
+
