b2e61e7f87506a6463aaa4f3caf9562e30f63231
Florencia Cabrera <aflorenciacabrera@gmail.com>
2019-06-13 20:47:18 -0300

    seccion 4 y 5

--- resources/views/encuesta.blade.php
+++ resources/views/encuesta.blade.php
@@ -73,7 +73,7 @@
                                 <div class="card-header" id="headingThree">
                                     <h2 class="mb-0">
                                         <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                            Sección #4
+                                            Sección #4 - IV. CARACTERÍSTICAS DE LOS MIEMBROS DEL HOGAR
                                         </button>
                                     </h2>
                                 </div>
@@ -89,7 +89,7 @@
                                 <div class="card-header" id="headingThree">
                                     <h2 class="mb-0">
                                     <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                        Sección #5
+                                        Sección #5 -ESTRATEGIAS DEL HOGAR
                                     </button>
                                     </h2>
                                 </div>
---
+++ resources/views/encuesta/seccion_5.blade.php
@@ -0,0 +1,214 @@
+
+
+<div class="form-group col-md-6">
+        <label for="numero_semana">En los últimos 3 meses, las personas de este hogar han vivido…</label>
+        <br>
+        <div class="form-row ">
+            <div class="col-md-10">
+                <label for="numero_semana">1… de lo que ganan en el trabajo?</label>
+            </div>
+            <div class="col-md-2" >
+                <select class="form-control" name="entrevista_realizada"  required>
+                        <option>Si</option>
+                        <option>No</option>
+                </select>
+            </div>
+        </div>
+        <div class="form-row ">
+            <div class="col-md-10">
+                <label for="numero_semana">2… de alguna jubilación o pensión</label>
+            </div>
+            <div class="col-md-2" >
+                <select class="form-control" name="entrevista_realizada"  required>
+                        <option>Si</option>
+                        <option>No</option>
+                </select>
+            </div>
+        </div>
+        <div class="form-row ">
+                <div class="col-md-10">
+                    <label for="numero_semana">3… de indeminización por despido?</label>
+                </div>
+                <div class="col-md-2" >
+                    <select class="form-control" name="entrevista_realizada"  required>
+                            <option>Si</option>
+                            <option>No</option>
+                    </select>
+                </div>
+        </div>
+        <div class="form-row ">
+                <div class="col-md-10">
+                    <label for="numero_semana">4… de seguro de desempleo?</label>
+                </div>
+                <div class="col-md-2" >
+                    <select class="form-control" name="entrevista_realizada"  required>
+                            <option>Si</option>
+                            <option>No</option>
+                    </select>
+                </div>
+        </div>
+        <div class="form-row ">
+                <div class="col-md-10">
+                    <label for="numero_semana">5… de subsidio o ayuda social (en dinero) del gobierno, iglesias, escuelas, etc.?</label>
+                </div>
+                <div class="col-md-2" >
+                    <select class="form-control" name="entrevista_realizada"  required>
+                            <option>Si</option>
+                            <option>No</option>
+                    </select>
+                </div>
+        </div>
+        <div class="form-row ">
+                <div class="col-md-10">
+                    <label for="numero_semana">6… con mercaderías, ropa, alimentos de familiares, vecinos u otras personas que no viven en este hogar? Cobraron…</label>
+                </div>
+                <div class="col-md-2" >
+                    <select class="form-control" name="entrevista_realizada"  required>
+                            <option>Si</option>
+                            <option>No</option>
+                    </select>
+                </div>
+        </div>
+        <div class="form-row ">
+                <div class="col-md-10">
+                    <label for="numero_semana">8… algún alquiler (por una vivienda, terreno, oficina, etc) de su propiedad?</label>
+                </div>
+                <div class="col-md-2" >
+                    <select class="form-control" name="entrevista_realizada"  required>
+                            <option>Si</option>
+                            <option>No</option>
+                    </select>
+                </div>
+        </div>
+        <div class="form-row ">
+                <div class="col-md-10">
+                    <label for="numero_semana">9… ganancias de algún negocio en el que no trabajan?</label>
+                </div>
+                <div class="col-md-2" >
+                    <select class="form-control" name="entrevista_realizada"  required>
+                            <option>Si</option>
+                            <option>No</option>
+                    </select>
+                </div>
+        </div>
+        <div class="form-row ">
+                <div class="col-md-10">
+                    <label for="numero_semana">10… intereses o rentas por plazos fijos/inversiones?</label>
+                </div>
+                <div class="col-md-2" >
+                    <select class="form-control" name="entrevista_realizada"  required>
+                            <option>Si</option>
+                            <option>No</option>
+                    </select>
+                </div>
+        </div>
+        <div class="form-row ">
+                <div class="col-md-10">
+                    <label for="numero_semana">11… una beca de estudio?</label>
+                </div>
+                <div class="col-md-2" >
+                    <select class="form-control" name="entrevista_realizada"  required>
+                            <option>Si</option>
+                            <option>No</option>
+                    </select>
+                </div>
+        </div>
+        <div class="form-row ">
+                <div class="col-md-10">
+                    <label for="numero_semana">12… cuotas de alimentos o ayuda en dinero de personas que no viven en el hogar? Además, han tenido que…</label>
+                </div>
+                <div class="col-md-2" >
+                    <select class="form-control" name="entrevista_realizada"  required>
+                            <option>Si</option>
+                            <option>No</option>
+                    </select>
+                </div>
+        </div>
+        <div class="form-row ">
+                <div class="col-md-10">
+                    <label for="numero_semana">13… gastar lo que tenían ahorrado?</label>
+                </div>
+                <div class="col-md-2" >
+                    <select class="form-control" name="entrevista_realizada"  required>
+                            <option>Si</option>
+                            <option>No</option>
+                    </select>
+                </div>
+        </div>
+        <div class="form-row ">
+                <div class="col-md-10">
+                    <label for="numero_semana">14… pedir préstamos a familiares/amigos?</label>
+                </div>
+                <div class="col-md-2" >
+                    <select class="form-control" name="entrevista_realizada"  required>
+                            <option>Si</option>
+                            <option>No</option>
+                    </select>
+                </div>
+        </div>
+        <div class="form-row ">
+                <div class="col-md-10">
+                    <label for="numero_semana">15… pedir préstamos a bancos, financieras, etc?</label>
+                </div>
+                <div class="col-md-2" >
+                    <select class="form-control" name="entrevista_realizada"  required>
+                            <option>Si</option>
+                            <option>No</option>
+                    </select>
+                </div>
+        </div>
+        <div class="form-row ">
+                <div class="col-md-10">
+                    <label for="numero_semana">16... Compran en cuotas o al fiado con tarjeta de crédito o libreta?</label>
+                </div>
+                <div class="col-md-2" >
+                    <select class="form-control" name="entrevista_realizada"  required>
+                            <option>Si</option>
+                            <option>No</option>
+                    </select>
+                </div>
+        </div>
+        <div class="form-row ">
+                <div class="col-md-10">
+                    <label for="numero_semana">17… Han tenido que vender alguna de sus pertenencicas?</label>
+                </div>
+                <div class="col-md-2" >
+                    <select class="form-control" name="entrevista_realizada"  required>
+                            <option>Si</option>
+                            <option>No</option>
+                    </select>
+                </div>
+        </div>
+        <div class="form-row ">
+                <div class="col-md-10">
+                    <label for="numero_semana">18… Tuvieron otros ingresos en efectivo? (limosnas, juegos de azar, etc.)</label>
+                </div>
+                <div class="col-md-2" >
+                    <select class="form-control" name="entrevista_realizada"  required>
+                            <option>Si</option>
+                            <option>No</option>
+                    </select>
+                </div>
+        </div>
+        <div class="form-row ">
+                <div class="col-md-10">
+                    <label for="numero_semana">19.1… Alguno de los niños (menores de 10 años) ayuda con algún dinero
+                            …trabajando?
+                            </label>
+                    <label for="numero_semana">19.2… Alguno de los niños (menores de 10 años) ayuda con algún dinero
+                                    …pidiendo?</label>
+                </div>
+                <div class="col-md-2" >
+                    <select class="form-control" name="entrevista_realizada"  required>
+                            <option>Si</option>
+                            <option>No</option>
+                    </select>
+                    <select class="form-control" name="entrevista_realizada"  required>
+                            <option>Si</option>
+                            <option>No</option>
+                    </select>
+                </div>
+        </div>
+       
+</div>
+
