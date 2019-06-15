c37ade41a47bb89edd6acd2788de2c7b1123686e
Florencia Cabrera <aflorenciacabrera@gmail.com>
2019-06-14 23:03:37 -0300

    seccion hogar

--- resources/views/encuesta/seccion_hogar_planesyprogramas.blade.php
+++ resources/views/encuesta/seccion_hogar_planesyprogramas.blade.php
@@ -4,109 +4,206 @@
 <label for="">HOGAR - PLANES Y PROGRAMAS GUBERNAMENTALES</label>
 <br>
 
-<div class="form-group col-md-6">
   <label for="numero_semana">En el mes de ….. ¿El hogar o alguno de los miembros del hogar recibió o fue beneficiario de….</label>
   <br>
+  <div class="card-body">
+                       
+    <div class="form-row">
+        <div class="form-group col-md-6">
   <div class="form-row ">
-      <div class="col-md-10">
+      <div class="col-md-6">
           <label for="numero_semana">1.Desayuno en institucion educativa?</label>
-      </div>
+         
+          <div class="col-md-10">
+            <label for="numero_semana">1.1¿Cuántos miembros?</label>
+            <input type="number" class="form-control">
+        </div>
+       
+    </div>
+     
       <div class="col-md-2" >
           <select class="form-control" name="entrevista_realizada"  required>
                   <option>Si</option>
                   <option>No</option>
           </select>
+      </div>
+    </div>
+    <br>
+    <div class="form-row ">
+        <div class="col-md-6">
+            <label for="numero_semana">2.Almuerzo en institucion educativa?</label>
+           
+            <div class="col-md-10">
+              <label for="numero_semana">2.1¿Cuántos miembros?</label>
+              <input type="number" class="form-control">
+          </div>
+         
+      </div>
+       
+        <div class="col-md-2" >
+            <select class="form-control" name="entrevista_realizada"  required>
+                    <option>Si</option>
+                    <option>No</option>
+            </select>
+        </div>
       </div>
       <br>
-      º
-  </div>
-  <div class="form-row ">
-      <div class="col-md-10">
-          <label for="numero_semana">2… de alguna jubilación o pensión</label>
+      <div class="form-row ">
+        <div class="col-md-6">
+            <label for="numero_semana">3.Merienda en institucion educativa?</label>
+           
+            <div class="col-md-10">
+              <label for="numero_semana">3.1¿Cuántos miembros?</label>
+              <input type="number" class="form-control">
+          </div>
+         
       </div>
-      <div class="col-md-2" >
-          <select class="form-control" name="entrevista_realizada"  required>
-                  <option>Si</option>
-                  <option>No</option>
-          </select>
+       
+        <div class="col-md-2" >
+            <select class="form-control" name="entrevista_realizada"  required>
+                    <option>Si</option>
+                    <option>No</option>
+            </select>
+        </div>
       </div>
-  </div>
-  <div class="form-row ">
-          <div class="col-md-10">
-              <label for="numero_semana">3… de indeminización por despido?</label>
+      <br>
+      <div class="form-row ">
+        <div class="col-md-6">
+            <label for="numero_semana">4.Tarifa social del agua?</label>
+           
+            <div class="col-md-10">
+              <label for="numero_semana">4.1 ¿Porcentaje?</label>
+              <input type="number" class="form-control">
           </div>
-          <div class="col-md-2" >
-              <select class="form-control" name="entrevista_realizada"  required>
-                      <option>Si</option>
-                      <option>No</option>
-              </select>
+         
+      </div>
+       
+        <div class="col-md-2" >
+            <select class="form-control" name="entrevista_realizada"  required>
+                    <option>Si</option>
+                    <option>No</option>
+            </select>
+        </div>
+      </div>
+      <br>
+      <div class="form-row ">
+        <div class="col-md-6">
+            <label for="numero_semana">5. Tarjeta verde?</label>
+           
+            <div class="col-md-10">
+              <label for="numero_semana">5.1 ¿Porcentaje?</label>
+              <input type="number" class="form-control">
           </div>
-  </div>
-  <div class="form-row ">
-          <div class="col-md-10">
-              <label for="numero_semana">4… de seguro de desempleo?</label>
+         
+      </div>
+       
+        <div class="col-md-2" >
+            <select class="form-control" name="entrevista_realizada"  required>
+                    <option>Si</option>
+                    <option>No</option>
+            </select>
+        </div>
+      </div>
+    </div>
+    <br>
+    <div class="form-group col-md-6">
+    
+      <div class="form-row ">
+        <div class="col-md-6">
+            <label for="numero_semana">6.Boleto estudiantil?</label>
+           
+            <div class="col-md-10">
+              <label for="numero_semana">6.1¿Cuántos miembros?</label>
+              <input type="number" class="form-control">
           </div>
-          <div class="col-md-2" >
-              <select class="form-control" name="entrevista_realizada"  required>
-                      <option>Si</option>
-                      <option>No</option>
-              </select>
+         
+      </div>
+       
+        <div class="col-md-2" >
+            <select class="form-control" name="entrevista_realizada"  required>
+                    <option>Si</option>
+                    <option>No</option>
+            </select>
+        </div>
+      </div>
+      <br>
+      <div class="form-row ">
+        <div class="col-md-6">
+            <label for="numero_semana">7. Tarjeta Mbareté?</label>
+           
+            <div class="col-md-10">
+              <label for="numero_semana">7.1¿Cuántos miembros?</label>
+              <input type="number" class="form-control">
           </div>
-  </div>
-  <div class="form-row ">
-          <div class="col-md-10">
-              <label for="numero_semana">5… de subsidio o ayuda social (en dinero) del gobierno, iglesias, escuelas, etc.?</label>
+         
+      </div>
+       
+        <div class="col-md-2" >
+            <select class="form-control" name="entrevista_realizada"  required>
+                    <option>Si</option>
+                    <option>No</option>
+            </select>
+        </div>
+      </div>
+      <br>
+      <div class="form-row ">
+        <div class="col-md-6">
+            <label for="numero_semana">8. Tarjeta Sapucay?</label>
+           
+            <div class="col-md-10">
+              <label for="numero_semana">8.1¿Cuántos miembros?</label>
+              <input type="number" class="form-control">
           </div>
-          <div class="col-md-2" >
-              <select class="form-control" name="entrevista_realizada"  required>
-                      <option>Si</option>
-                      <option>No</option>
-              </select>
+         
+      </div>
+       
+        <div class="col-md-2" >
+            <select class="form-control" name="entrevista_realizada"  required>
+                    <option>Si</option>
+                    <option>No</option>
+            </select>
+        </div>
+      </div>
+      <br>
+      <div class="form-row ">
+        <div class="col-md-6">
+            <label for="numero_semana">9. Plan mamá?</label>
+           
+            <div class="col-md-10">
+              <label for="numero_semana">9.1¿Cuántos miembros?</label>
+              <input type="number" class="form-control">
           </div>
-  </div>
-  <div class="form-row ">
-          <div class="col-md-10">
-              <label for="numero_semana">6… con mercaderías, ropa, alimentos de familiares, vecinos u otras personas que no viven en este hogar? Cobraron…</label>
+         
+      </div>
+       
+        <div class="col-md-2" >
+            <select class="form-control" name="entrevista_realizada"  required>
+                    <option>Si</option>
+                    <option>No</option>
+            </select>
+        </div>
+      </div>
+      <br>
+      <div class="form-row ">
+        <div class="col-md-6">
+            <label for="numero_semana">10. Asignación Universal por hijo?</label>
+           
+            <div class="col-md-10">
+              <label for="numero_semana">10.1 Monto cobrado total</label>
+              <input type="number" class="form-control">
           </div>
-          <div class="col-md-2" >
-              <select class="form-control" name="entrevista_realizada"  required>
-                      <option>Si</option>
-                      <option>No</option>
-              </select>
-          </div>
-  </div>
-  <div class="form-row ">
-          <div class="col-md-10">
-              <label for="numero_semana">8… algún alquiler (por una vivienda, terreno, oficina, etc) de su propiedad?</label>
-          </div>
-          <div class="col-md-2" >
-              <select class="form-control" name="entrevista_realizada"  required>
-                      <option>Si</option>
-                      <option>No</option>
-              </select>
-          </div>
-  </div>
-  <div class="form-row ">
-          <div class="col-md-10">
-              <label for="numero_semana">9… ganancias de algún negocio en el que no trabajan?</label>
-          </div>
-          <div class="col-md-2" >
-              <select class="form-control" name="entrevista_realizada"  required>
-                      <option>Si</option>
-                      <option>No</option>
-              </select>
-          </div>
-  </div>
-  <div class="form-row ">
-          <div class="col-md-10">
-              <label for="numero_semana">10… intereses o rentas por plazos fijos/inversiones?</label>
-          </div>
-          <div class="col-md-2" >
-              <select class="form-control" name="entrevista_realizada"  required>
-                      <option>Si</option>
-                      <option>No</option>
-              </select>
-          </div>
-  </div>
+         
+      </div>
+       
+        <div class="col-md-2" >
+            <select class="form-control" name="entrevista_realizada"  required>
+                    <option>Si</option>
+                    <option>No</option>
+            </select>
+        </div>
+      </div>
+    </div>
 </div>
+</div>
+
 {{-- ------------------------------------ --}}
