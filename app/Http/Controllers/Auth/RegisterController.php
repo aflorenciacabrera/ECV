c8725ea6df8c009883cf02043e855c39eb8ddfd0
aflorenciacabrera <aflorenciacabrera@gmail.com>
2019-06-23 21:05:27 -0300

    rl

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\User;
 use App\Role;
-use App\Http\Controllers\Auth\Role;
+//use App\Http\Controllers\Auth\Role;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
