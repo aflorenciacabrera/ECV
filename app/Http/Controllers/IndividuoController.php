1abbd66aaa65de81f85d3035989b3d2511336caa
Florencia Cabrera <aflorenciacabrera@gmail.com>
2019-07-02 18:15:55 -0300

    encues ind

--- app/Http/Controllers/IndividuoController.php
+++ app/Http/Controllers/IndividuoController.php
@@ -10,4 +10,16 @@ class IndividuoController extends Controller
     public function verEncuestaIndividuo(){
         return view('encuestaIndividuo');
       }
+
+        public function crearEncuestaHogar (Request $request)
+        {
+          $i = new hogar; 
+  
+        
+          // $h->numero_encuestador= $request->numero_encuestador;
+  
+          $i->save();
+       
+        return redirect(url('home'))->with('status','Formulario de Encuensta Individuo cargado');;
+        }
 }
--- resources/views/encuestaIndividuo.blade.php
+++ resources/views/encuestaIndividuo.blade.php
@@ -1,4 +1,46 @@
 @extends('layouts.app')
 @section('content')
-
+<div class="container-fluid">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header ">
+                        <label for="" class="d-flex justify-content-center" >Cargar Encuesta de Vivienda  </label>                            
+                </div>
+                <div class="card-body">
+                    {{-- Alert mesaje --}}
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                     {{-- <form method="POST" action="{{url('encuesta/Vivienda/crear')}}"> --}}
+                    <form method="POST" action="{{route('crearEncuestaVivienda')}}">
+                        @csrf
+                        <div class="accordion" id="accordionExample">
+                            <div class="card">
+                                <div class="card-header" id="headingOne">
+                                    <h2 class="mb-0">
+                                        <button class="btn btn-secondary" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                                        Sección #1 - I. IDENTIFICACIÓN
+                                        </button>
+                                    </h2>
+                                </div>
+                                <div id="collapseOne" class="collapse " aria-labelledby="headingOne" data-parent="#accordionExample">
+                                    <div class="card-body">
+                                        @include('encuestaIndividuo.seccion_1')
+                                    </div>
+                              </div>
+                            </div>
+                        
+                            <div class="card-footer text-muted d-flex justify-content-end">
+                                <button class="btn btn-success" type="submit">Cargar</button>  
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
