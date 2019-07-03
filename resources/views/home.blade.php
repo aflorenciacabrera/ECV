e7dd5b2f860f0648d1f3e7e56f358267bb038c23
Florencia Cabrera <aflorenciacabrera@gmail.com>
2019-07-02 23:49:59 -0300

    logos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,13 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">
+            {{-- <div class="card"> --}}
+                {{-- <div class="card-header">
                    <h3> <p>Encuesta de Calidad de Vida</p></h3>
                         
-                </div>
+                </div> --}}
 
-                <div class="card-body">
+                {{-- <div class="card-body"> --}}
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
@@ -22,21 +22,18 @@
                     @else
                         <div>Acceso Encuestador</div>
                     @endif --}}
-                    <div class="jumbotron">
-                            {{-- <h1 class="display-4">ECV!</h1> --}}
-                            {{-- <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p> --}}
-                            {{-- <hr class="my-4"> --}}
-                            
-                            {{-- <a href="{{route('encuesta')}}" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Primary link</a> --}}
-                            <a href="{{route('verEncuestaVivienda')}}" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true"> Encuesta Vivienda</a>
-                            <a href="{{route('verEncuestaHogar')}}" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true"> Encuesta Hogar</a>
-                            <a href="{{route('verEncuestaIndividuo')}}" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true"> Encuesta Individuo</a>
-                          </div>
-                    {{-- <input type="datetime-local" name="fechayhora"  step="1"  >
-                    Fecha y hora actual: <input type="datetime-local" name="fechahora" step="1"  value=""> --}}
-                    
-                </div>
-            </div>
+                    <div class="jumbotron ">
+                        <h1 class="display-5 text-center">Menú</h1>
+                            <p class="lead"></p>
+                            <hr class="my-4">
+                        <div class="row justify-content-center">
+                            <a href="{{route('verEncuestaVivienda')}}" role="button" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true"> Encuesta Vivienda</a> &nbsp;
+                            <a href="{{route('verEncuestaHogar')}}" role="button" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true"> Encuesta Hogar</a>&nbsp;
+                            <a href="{{route('verEncuestaIndividuo')}}" role="button" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true"> Encuesta Individuo</a>
+                        </div>
+                    </div>
+                {{-- </div> --}}
+            {{-- </div> --}}
         </div>
     </div>
 </div>
