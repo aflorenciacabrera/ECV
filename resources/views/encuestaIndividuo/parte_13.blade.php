8e7d0a91319234b97d83b9799de1ddbed6a0db35
Sambrana Ivan <sambranaivan@gmail.com>
2019-07-15 20:37:54 -0300

    seccion_13

--- resources/views/encuestaIndividuo/parte_13.blade.php
+++ resources/views/encuestaIndividuo/parte_13.blade.php
@@ -1,92 +1,76 @@
 {{-- @extends('layouts.app')
 @section('content') --}}
-<div class="card-body">      
-        <div class="form-row">
-            <div class="form-group col-md-6">
-                    <label for="codigo_area">1a. En la semana pasada, <input type="text" class=" col-md-3" name=""> trabajó por lo menos una hora? </label><small id="" class="form-text text-muted">(sin contar las tareas de su hogar) </small>
-                     <select class="form-control" name=""  >
-                                <option value="1">Si</option>
-                                <option value="2" >No</option>             
-                        </select>
-                    {{-- <small id="codigo_area" class="form-text text-muted"> </small> --}}
-            </div>   
-             <div class="form-group col-md-6">
-                    <label for="codigo_area">1b. En esa semana, ¿hizo alguna changa, fabricó algo para vender, ayudó a un familiar/amigo en su negocio?</label>
-                     <select class="form-control" name=""  >
-                                <option value="1">Si</option>
-                                <option value="2" >No</option>             
-                        </select>
-                        
-                    {{-- <small id="codigo_area" class="form-text text-muted"> </small> --}}
-            </div>           
-             
-        </div> 
-        <div class="form-row">
-           <div class="form-group col-md-6">
-                    <label for="codigo_area">1c. ¿Es un trabajo pago, en dinero o en especie?</label>
-                     <select class="form-control" name=""  ><small id="" class="form-text text-muted"> </small>
-                                <option value="1">Si</option>
-                                <option value="2" >No</option>             
-                        </select>
-                    {{-- <small id="codigo_area" class="form-text text-muted"> </small> --}}
-            </div>    
-             <div class="form-group col-md-6">
-                    <label for="codigo_area">1d. ¿Trabajó…</label>
-                     <select class="form-control" name=""  >
-                                <option value="1">… con un familiar/amigo en su negocio, taller chacra?</option>
-                                <option value="2" >… como trabajador ad-honorem?(aprendiz, meritorio judicial, etc)</option>
-                                <option value="3">… de otra forma? (especificar)</option>             
-                        </select>
-                        <input type="text" class="form-control" name=""> 
-                    {{-- <small id="codigo_area" class="form-text text-muted"> </small> --}}
-            </div>    
-        </div>   
-         <div class="form-row">
-           <div class="form-group col-md-6">
-                    <label for="codigo_area">1e. La semana pasada…</label>
-                     <select class="form-control" name=""  ><small id="" class="form-text text-muted"> </small>
-                                <option value="1">… no deseaba/no quería trabajar?</option>
-                                <option value="2" >… no podía trabajar por razones personales? (cuidado del hogar, estudios, incapacidad)</option>
-                                <option value="3" >… no tenía/ no conseguía trabajo?</option>
-                                <option value="4" >… no tuvo pedidos/clientes?</option>
-                                <option value="5" >tenía un trabajo/negocio al que no concurrió?</option>             
-                        </select>
-                    {{-- <small id="codigo_area" class="form-text text-muted"> </small> --}}
-            </div>    
-             <div class="form-group col-md-6">
-                    <label for="codigo_area">1f- ¿No concurrió por….</label>
-                     <select class="form-control" name=""  >
-                                <option value="1">… vacaciones, licencia? (enfermedad, matrimonio, embarazo, etc.)</option>
-                                <option value="2" >… causas personales? (viajes, trámites, etc)</option>
-                                <option value="3">… huelga/conflicto laboral?</option>    
-                                <option value="4">… Suspensión?</option>  
-                                <option value="5">… otras causas laborales? (rotura de equipos, falta de materias primas, mal tiempo)</option>           
-                        </select>
-                        <input type="text" class="form-control" name=""> 
-                    {{-- <small id="codigo_area" class="form-text text-muted"> </small> --}}
-            </div>    
-        </div>       
-        <div class="form-row">
-            <div class="form-group col-md-6">
-                    <label for="codigo_area">1g. ¿Le mantienen el pago durante la suspensión? </label>
-                     <select class="form-control" name=""  >
-                                <option value="1">Si</option>
-                                <option value="2" >No</option> 
-                                <option value="3" >NS/Nr</option>             
-                        </select>
-                    {{-- <small id="codigo_area" class="form-text text-muted"> </small> --}}
-            </div>   
-             <div class="form-group col-md-6">
-                    <label for="codigo_area">1h. ¿Volverá a ese trabajo a lo sumo en un mes?</label>
-                      <select class="form-control" name=""  >
-                                <option value="1">Si</option>
-                                <option value="2" >No</option> 
-                                <option value="3" >NS/Nr</option>             
-                        </select>
-                        
-                    {{-- <small id="codigo_area" class="form-text text-muted"> </small> --}}
-            </div>           
-             
-        </div>                   
+<div class="card-body border">
+       <div class="form-group">
+         <label for="">Observaciones</label>
+         <textarea class="form-control" name="seccion_13_observaciones" id="" rows="3"></textarea>
+       </div>
+       <div class="row border">
+        <div class="col-md-4 border ">
+           <div class="row">
+                <div class="col-md-6 form-inline">
+                    <label for="seccion_13_encuestador">Encuestador</label>
+                    <input type="text" name="seccion_13_encuestador" id="seccion_13_encuestador" class="form-control inline">
+            </div>
+            <div class="col-md-6 form-inline">
+                <label for="seccion_13_encuestador_numero">N°</label>
+                    <input type="number" min=0 step=1 name="seccion_13_encuestador_numero" id="seccion_13_encuestador_numero" class="form-control inline">
+            </div>
+
+           </div>
+
+
+
+        </div>
+        <div class="col-md-4">
+            <div class="row border">
+                <div class="col-md-12 text-center">Visitas</div>
+                <div class="col-md-6 text-center">Fecha</div>
+                <div class="col-md-6 text-center">Hora</div>
+                <div class="col-md-6">
+                    <div class="form-group form-inline">
+                      <label for="seccion_13_fecha1">1°</label>
+                      <input type="date" class="form-control form-control-sm" name="seccion_13_fecha1" id="seccion_13_fecha1">
+                    </div>
+                </div>
+                <div class="col-md-6">
+                     <input type="time" class="form-control form-control-sm" name="seccion_13_hora1" id="seccion_13_hora1">
+                </div>
+                <div class="col-md-6">
+                    <div class="form-group form-inline">
+                      <label for="seccion_13_fecha2">2°</label>
+                      <input type="date" class="form-control form-control-sm" name="seccion_13_fecha2" id="seccion_13_fecha2">
+                    </div>
+                </div>
+                <div class="col-md-6">
+                     <input type="time" class="form-control form-control-sm" name="seccion_13_hora2" id="seccion_13_hora2">
+                </div>
+
+                <div class="col-md-6">
+                    <div class="form-group form-inline">
+                      <label for="seccion_13_fecha3">3°</label>
+                      <input type="date" class="form-control form-control-sm" name="seccion_13_fecha3" id="seccion_13_fecha3">
+                    </div>
+                </div>
+                <div class="col-md-6">
+                     <input type="time" class="form-control form-control-sm" name="seccion_13_hora3" id="seccion_13_hora3">
+                </div>
+
+
+            </div>
+        </div>
+        <div class="col-md-4 border">
+            <div class="form-group">
+              <label for="seccion_13_modalidad">Modalidad de Aplicación</label>
+              <select class="form-control" name="seccion_13_modalidad" id="">
+                <option value=""></option>
+                <option value="1">1. Persona Completa</option>
+                <option value="2">2. Persona Telefónica</option>
+                <option value="3">3. Sólo Telefónica</option>
+              </select>
+            </div>
+        </div>
+       </div>
+
 </div>
 {{-- @endsection --}}
