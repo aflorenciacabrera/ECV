633efa44fac97ef578b4e3de80545d5143760e84
Sambrana Ivan <sambranaivan@gmail.com>
2019-07-17 20:25:21 -0300

    revision de hogar + nros en select

--- resources/views/encuestaHogar/seccion_razones_no_encuesta.blade.php
+++ resources/views/encuestaHogar/seccion_razones_no_encuesta.blade.php
@@ -5,32 +5,32 @@
             <div class="form-group col-md-6">
                 <div class="form-row ">
                         <div class="col-md-6">
-                                <select name="motivo" id="" class="form-control">
+                                <select name="motivo" id="" class="form-control form-control-sm">
                                     <option value=""></option>
-                                        <option value="">Ausencia</option>
-                                        <option value="">Rechazo</option>
-                                        <option value="">Otras causas</option>
+                                        <option value="7">1. Ausencia</option>
+                                        <option value="8">2. Rechazo</option>
+                                        <option value="9">9. Otras causas</option>
                                 </select>
                         </div>
                             <div class="col-md-6" >
-                                    <select name="ausencia" id="" class="form-control">
+                                    <select name="ausencia" id="" class="form-control form-control-sm">
                                         <option value=""></option>
-                                            <option value="">No se pudo contactar en tres visitas</option>
-                                            <option value="">Por causas circunstanciales</option>
-                                            <option value="">Viaje</option>
-                                            <option value="">Vacaciones</option>
+                                            <option value="1">1. No se pudo contactar en tres visitas</option>
+                                            <option value="2">2. Por causas circunstanciales</option>
+                                            <option value="3">3. Viaje</option>
+                                            <option value="4">4. Vacaciones</option>
                                     </select>
-                                    <select name="rechazo" id="" class="form-control">
+                                    <select name="rechazo" id="" class="form-control form-control-sm">
                                         <option value=""></option>
-                                            <option value="">Negativa rotunda</option>
-                                            <option value="">Rechazo por portero eléctrico</option>
-                                            <option value="">Se acordaron entrevistas que no se concretaron</option>
+                                            <option value="1">1. Negativa rotunda</option>
+                                            <option value="2">2. Rechazo por portero eléctrico</option>
+                                            <option value="3">3. Se acordaron entrevistas que no se concretaron</option>
                                     </select>
-                                    <select name="otro" id="" class="form-control">
+                                    <select name="otro" id="" class="form-control form-control-sm">
                                         <option value=""></option>
-                                            <option value="">Duelo</option>
-                                            <option value="">Alcoholismo, discapcidad, idioma extranjero</option>
-                                            <option value="">Problema de seguridad</option>
+                                            <option value="1">1. Duelo</option>
+                                            <option value="2">2. Alcoholismo, discapcidad, idioma extranjero</option>
+                                            <option value="3">3. Problema de seguridad</option>
                                     </select>
                             </div>
                     </div>
@@ -39,14 +39,14 @@
                                 <label for="">INFORMANTE:</label>
                             </div>
 
-                                    <input type="text" name="informante" class="form-control">
+                                    <input type="text" name="informante" class="form-control form-control-sm">
 
                     </div>
                     <div class="form-row ">
                             <div class="col-md-6">
                                 <label for="">OBSERVACIONES:</label>
                             </div>
-                                <textarea name="observaciones" id="" cols="30" rows="6" class="form-control"></textarea>
+                                <textarea name="observaciones" id="" cols="30" rows="6" class="form-control form-control-sm"></textarea>
 
                     </div>
             </div>
