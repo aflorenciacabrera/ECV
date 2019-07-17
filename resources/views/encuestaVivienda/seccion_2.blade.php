748024781628133410aba79065536ccf0afb3dc0
Sambrana Ivan <sambranaivan@gmail.com>
2019-07-17 19:19:39 -0300

    patron s/n para calle

--- resources/views/encuestaVivienda/seccion_2.blade.php
+++ resources/views/encuestaVivienda/seccion_2.blade.php
@@ -12,34 +12,34 @@
             </div>
             <div class="form-group col-md-4">
                         <label for="calle">Calle:</label>
-                        <input type="text" class="form-control" id="calle" name="calle" placeholder="" required>
+                        <input type="text" class="form-control" id="calle" name="calle"  pattern="[1-9]*|s\/n|S\/N" title="custom message" placeholder="" required>
            </div>
         </div>
 
         <div class="form-row">
                 <div class="form-group col-md-2">
                         <label for="numero">N°:</label>
-                        <input type="number"  class="form-control" id="numero" name="numero" placeholder=""  required>
+                        <input type="number"  class="form-control" id="numero" name="numero" placeholder=""  >
                 </div>
                 <div class="form-group col-md-2">
                         <label for="piso">Piso:</label>
-                        <input type="text" class="form-control" id="piso" name="piso" placeholder="" required >
+                        <input type="text" class="form-control" id="piso" name="piso" placeholder=""  >
                 </div>
                 <div class="form-group col-md-2">
                         <label for="deptoocasa">Depto o casa:</label>
-                        <input type="text" class="form-control" id="deptoocasa" name="deptoocasa" placeholder="" required >
+                        <input type="text" class="form-control" id="deptoocasa" name="deptoocasa" placeholder=""  >
                 </div>
                 <div class="form-group col-md-2">
                         <label for="habitacion">Habitación:</label>
-                        <input type="text" class="form-control" id="habitacion" name="habitacion" placeholder=""  required>
+                        <input type="text" class="form-control" id="habitacion" name="habitacion" placeholder=""  >
                 </div>
                 <div class="form-group col-md-2">
                         <label for="tipoVivienda">Tipo de vivienda:</label>
-                        <input type="text" class="form-control" id="tipoVivienda" name="tipoVivienda" placeholder=""  required>
+                        <input type="text" class="form-control" id="tipoVivienda" name="tipoVivienda" placeholder=""  >
                 </div>
                 <div class="form-group col-md-2">
                         <label for="descripcion">Descripción:</label>
-                        <input type="text" class="form-control" id="descripcion" name="descripcion" placeholder="" required >
+                        <input type="text" class="form-control" id="descripcion" name="descripcion" placeholder=""  >
                 </div>
         </div>
     </div>
