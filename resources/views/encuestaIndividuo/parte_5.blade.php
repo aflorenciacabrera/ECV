56e8d6cd871ede5e09b0ff27ff65d79d00da6f39
Sambrana Ivan <sambranaivan@gmail.com>
2019-07-23 15:15:02 -0300

    numeros en los sele

--- resources/views/encuestaIndividuo/parte_5.blade.php
+++ resources/views/encuestaIndividuo/parte_5.blade.php
@@ -1,14 +1,14 @@
 {{-- @extends('layouts.app')
 @section('content') --}}
-<div class="card-body">      
+<div class="card-body">
         <div class="form-row">
             <div class="form-group col-md-6">
                     <label for="PP05A">5a. ¿Ese trabajo, lo hace… </label>
-                         <select class="form-control" name="PP05A" id="PP05A" >
+                         <select class="form-control" name="PP05A" id="PP05A">
                                 <option value=""> </option>
-                                <option value="1">… para su propio negocio/ empresa/ actividad?</option>
-                                <option value="2" >… para el negocio/ empresa/ actividad de un familiar?</option>  
-                                <option value="3">… o como obrero o empleado para un patrón / empresa institución? (incluye agencia de empleo) </option>           
+                                <option value="1">1. … para su propio negocio/ empresa/ actividad?</option>
+                                <option value="2">2. … para el negocio/ empresa/ actividad de un familiar?</option>
+                                <option value="3">3. … o como obrero o empleado para un patrón / empresa institución? (incluye agencia de empleo) </option>
                         </select>
                         <br>
                  <label for=" ">5c. ¿En ese negocio/empresa/actividad, tiene…</label>
@@ -18,10 +18,10 @@
                         </div>
                         <div class="form-group col-md-4">
                                 <select class="form-control" name="PP05C_1" id="PP05C_1">
-                                        <option value=" " ></option>
-                                        <option value="1" >Propio (del negocio)</option>     
-                                        <option value="2" >Prestado/ Alquilado</option>    
-                                        <option value="3" >Notiene</option>          
+                                        <option value=" "> </option>
+                                        <option value="1">1. Propio (del negocio)</option>
+                                        <option value="2">2. Prestado/ Alquilado</option>
+                                        <option value="3">3. Notiene</option>
                                 </select>
                         </div>
                     </div>
@@ -31,10 +31,10 @@
                         </div>
                         <div class="form-group col-md-4">
                                 <select class="form-control" name="PP05C_2" id="PP05C_2">
-                                        <option value=" " ></option>
-                                        <option value="1" >Propio (del negocio)</option>     
-                                        <option value="2" >Prestado/ Alquilado</option>    
-                                        <option value="3" >Notiene</option>                
+                                        <option value=" "> </option>
+                                        <option value="1">1. Propio (del negocio)</option>
+                                        <option value="2">2. Prestado/ Alquilado</option>
+                                        <option value="3">3. Notiene</option>
                                 </select>
                         </div>
                      </div>
@@ -44,97 +44,97 @@
                         </div>
                         <div class="form-group col-md-4">
                                 <select class="form-control" name="PP05C_3" id="PP05C_3">
-                                <option value=" " ></option>
-                                <option value="1" >Propio (del negocio)</option>     
-                                <option value="2" >Prestado/ Alquilado</option>    
-                                <option value="3" >Notiene</option>                 
+                                <option value=" "> </option>
+                                <option value="1">1. Propio (del negocio)</option>
+                                <option value="2">2. Prestado/ Alquilado</option>
+                                <option value="3">3. Notiene</option>
                                 </select>
                         </div>
                 </div>
-                      
-            </div>   
+
+            </div>
              <div class="form-group col-md-6">
                 <label for="PP05B">5b. ¿Por ese trabajo le pagan sueldo (en dinero o en especie)?</label>
                 <select class="form-control" name="PP05B" id="PP05B">
                         <option value=""> </option>
-                        <option value="1">Si</option>
-                        <option value="2" >No</option>             
-                </select>  
+                        <option value="1">1. Si</option>
+                        <option value="2">2. No</option>
+                </select>
                 <br>
                 <label for="PP05B1">5b1. ¿Por ese trabajo…</label>
                 <select class="form-control" name="PP05B1" id="PP05B1">
                         <option value=""> </option>
-                        <option value="1">… retira dinero?</option>
-                        <option value="2" >… retira sólo mercaderías?</option>  
-                        <option value="3" >… no retira nada?</option>           
-                </select> 
+                        <option value="1">1. … retira dinero?</option>
+                        <option value="2">2. … retira sólo mercaderías?</option>
+                        <option value="3">3. … no retira nada?</option>
+                </select>
                 <br>
                 <label for="">5b2. ¿Cuánto tiempo hace que trabaja en ese empleo en forma continua?</label>
                  <div class="form-row">
                                 <div class="form-group col-md-4">
                                 <div class="text-center"><label for="PP05B2_ANO">Año</label></div>
-                                <input type="number" class="form-control" name="PP05B2_ANO" id="PP05B2_ANO" step="1" min="0" max="99"></div> 
+                                <input type="number" class="form-control" name="PP05B2_ANO" id="PP05B2_ANO" step="1" min="0" max="99"></div>
                                 <div class="form-group col-md-4">
                                 <div class="text-center"><label for="PP05B_MES">Mes</label></div>
-                                <input type="number" class="form-control" name="PP05B_MES" id="PP05B_MES" step="1" min="0" max="99"></div> 
+                                <input type="number" class="form-control" name="PP05B_MES" id="PP05B_MES" step="1" min="0" max="99"></div>
                                 <div class="form-group col-md-4">
                                 <div class="text-center"><label for="PP05B2_DIA">Día</label></div>
-                                <input type="number" class="form-control" name="PP05B2_DIA" id="PP05B2_DIA" step="1" min="0" max="99"></div> 
-                        </div>  
-            </div>           
-        </div> 
+                                <input type="number" class="form-control" name="PP05B2_DIA" id="PP05B2_DIA" step="1" min="0" max="99"></div>
+                        </div>
+            </div>
+        </div>
         <div class="form-row">
            <div class="form-group col-md-6">
                 <label for="PP05D">5d. ¿En ese negocio/empresa/actividad, se emplean personas asalariadas?</label>
-                <select class="form-control" name="PP05D" id="PP05D" >
-                        <option value=" " ></option>
-                        <option value="1">Sí, siempre</option>
-                        <option value="2" >Sólo a veces por temporada</option>
-                        <option value="3">No emplea/ no contrata personal</option>             
+                <select class="form-control" name="PP05D" id="PP05D">
+                        <option value=" "> </option>
+                        <option value="1">1. Sí, siempre</option>
+                        <option value="2">2. Sólo a veces por temporada</option>
+                        <option value="3">3. No emplea/ no contrata personal</option>
                 </select>
-            </div>    
-        
+            </div>
+
              <div class="form-group col-md-6">
                     <label for="PP05E">5e. ¿Para la actividad del negocio, en los últimos 3 meses, tuvo que gastar en la compra de materias primas/mercaderías/productos, pagar servicios u otros gastos?</label>
-                     <select class="form-control" name="PP05E" id="PP05E" >
-                        <option value=" " ></option>
-                        <option value="1">Si</option>
-                        <option value="2">No</option>             
+                     <select class="form-control" name="PP05E" id="PP05E">
+                        <option value=" "> </option>
+                        <option value="1">1. Si</option>
+                        <option value="2">2. No</option>
                         </select>
-            </div>    
-        </div>   
+            </div>
+        </div>
          <div class="form-row">
            <div class="form-group col-md-6">
                     <label for="PP05F">5f. ¿Ese negocio/empresa/actividad, trabaja habitualmente para…</label>
-                     <select class="form-control" name="PP05F" id="PP05F"  >
-                                <option value=" " ></option>
-                                <option value="6">… un solo cliente? (persona, empresa)</option>
-                                <option value="7" >distintos clientes? (incluye público en general)</option>         
+                     <select class="form-control" name="PP05F" id="PP05F" >
+                                <option value=" "> </option>
+                                <option value="6">6. … un solo cliente? (persona, empresa)</option>
+                                <option value="7">7. distintos clientes? (incluye público en general)</option>
                         </select>
-                   
-            </div>    
+
+            </div>
              <div class="form-group col-md-6">
                     <label for="PP05G">5g. ¿Ese cliente es… </label>
-                     <select class="form-control" name="PP05G" id="PP05G"  >
-                                <option value=" " ></option>
-                                <option value="1">… una empresa/negocio/institución?</option>
-                                <option value="2" >… una familia/hogar?</option>       
+                     <select class="form-control" name="PP05G" id="PP05G" >
+                                <option value=" "> </option>
+                                <option value="1">1. … una empresa/negocio/institución?</option>
+                                <option value="2">2. … una familia/hogar?</option>
                         </select>
-            </div>   
+            </div>
              <div class="form-group col-md-6">
                     <label for="PP05H">5h. ¿Durante cuánto tiempo ha estado trabajando en ese empleo en forma continua?</label>
                     <small id="PP05H" class="form-text text-muted"> (Con interrupciones laborales no mayores de 15 días) (no lea, escuche y marque la que corresponda)</small>
-                        <select class="form-control" name="PP05H" id="PP05H"  >
-                                <option value=" " ></option>
-                                <option value="1">Menos de 1 mes</option>
-                                <option value="2" >De 1 a 3 meses</option> 
-                                <option value="3" >Más de 3 meses y menos de 6 meses</option>  
-                                <option value="4" >Más de 6 meses y menos de 1 año</option> 
-                                <option value="5" >Más de 1 año y menos de 5 años</option> 
-                                <option value="6" >Más de 5 años</option> 
-                                <option value="9" >NS/Nr</option>         
+                        <select class="form-control" name="PP05H" id="PP05H" >
+                                <option value=" "> </option>
+                                <option value="1">1. Menos de 1 mes</option>
+                                <option value="2">2. De 1 a 3 meses</option>
+                                <option value="3">3. Más de 3 meses y menos de 6 meses</option>
+                                <option value="4">4. Más de 6 meses y menos de 1 año</option>
+                                <option value="5">5. Más de 1 año y menos de 5 años</option>
+                                <option value="6">6. Más de 5 años</option>
+                                <option value="9">9. NS/Nr</option>
                         </select>
-            </div>           
-        </div>                     
+            </div>
+        </div>
 </div>
 {{-- @endsection --}}
--- resources/views/encuestaIndividuo/parte_6.blade.php
+++ resources/views/encuestaIndividuo/parte_6.blade.php
@@ -3,10 +3,10 @@
           <div class="row justify-content-center">
                 <div class="display5 text-center col-md-6">
                         <label for="PP06A">6a. En ese negocio/empresa/actividad ¿tiene socios o familiares asociados?</label>
-                        <select class="form-control" name="PP06A" id="PP06A" >
+                        <select class="form-control" name="PP06A" id="PP06A">
                                 <option value=" "> </option>
-                                <option value="1">Si</option>
-                                <option value="2" >No</option>
+                                <option value="1">1. Si</option>
+                                <option value="2">2. No</option>
                         </select>
                 </div>
           </div>
@@ -19,10 +19,10 @@
              <div class="form-group col-md-6">
                     <label for="PP06C">6c. Por el mes de…………………. ¿Cuánto ganó en su negocio/empresa/actividad?</label>
                     <input type="number" class="form-control" name="PP06C" id="PP06C" step="1" min="0" max="9999999999">
-                     <select class="form-control" name="PP06C_op" id="PP06C_op" >
-                                 <option value=" "> </option>
-                                <option value="-8">No tuvo ingresos</option>
-                                <option value="-7" >No tenía esa ocupación</option>
+                     <select class="form-control" name="PP06C_op" id="PP06C_op">
+                                 <option value=" ">  </option>
+                                <option value="8">8. No tuvo ingresos</option>
+                                <option value="7">7. No tenía esa ocupación</option>
                         </select>
 
             </div>
@@ -33,58 +33,58 @@
                     <label for="PP06D">6d. Por el mes………………………… ¿cuánto ganó…………. en su negocio/empresa/actividad considerando sólo la parte que le corresponde a el/ella</label>
                     <small id="PP06D" class="form-text text-muted">(descontando los gastos) </small>
                     <input type="number" class="form-control" name="PP06D" id="PP06D" step="1" min="0" max="9999999999">
-                     <select class="form-control" name="PP06D_op" id="PP06D_op" >
+                     <select class="form-control" name="PP06D_op" id="PP06D_op">
                                  <option value=" "> </option>
-                                <option value="-8">No tuvo ingresos</option>
-                                <option value="-7" >No tenía esa ocupación</option>
+                                <option value="8">8. No tuvo ingresos</option>
+                                <option value="7">7. No tenía esa ocupación</option>
                         </select>
 
             </div>
              <div class="form-group col-md-6">
                     <label for="PP06E">6e. Ese negocio/empresa/actividad….es una sociedad jurídicamente constituída?</label>
-                     <select class="form-control" name="PP06E" id="PP06E" >
-                              <option value=" "> </option>
-                                <option value="1">(S.A., S.R.L., comandita por acciones, etc.)</option>
-                                <option value="2" >… es una sociedad de otra forma legal?</option>
-                                <option value="3">… o es una sociedad convenida de palabra?</option>
+                     <select class="form-control" name="PP06E" id="PP06E">
+                              <option value=" ">   </option>
+                                <option value="1">1. (S.A., S.R.L., comandita por acciones, etc.)</option>
+                                <option value="2">2. … es una sociedad de otra forma legal?</option>
+                                <option value="3">3. … o es una sociedad convenida de palabra?</option>
                         </select>
             </div>
         </div>
          <div class="form-row">
            <div class="form-group col-md-6">
                     <label for="PP06F">6f. ¿Tiene sueldo asignado?</label>
-                     <select class="form-control" name="PP06F" id="PP06F" >
-                              <option value=" "> </option>
-                                <option value="1">Si</option>
-                                <option value="2" >No</option>
+                     <select class="form-control" name="PP06F" id="PP06F">
+                              <option value=" ">  </option>
+                                <option value="1">1. Si</option>
+                                <option value="2">2. No</option>
                         </select>
 
             </div>
              <div class="form-group col-md-6">
                     <label for="PP06G">6g. ¿Cuánto cobró por el mes de ………….….… como sueldo?</label>
                      <input type="number" class="form-control" name="PP06G" id="PP06G" step="1" min="0" max="9999999999">
-                     <select class="form-control" name="PP06G_op" id="PP06G_op" >
-                                <option value="-8">No cobró nada</option>
+                     <select class="form-control" name="PP06G_op" id="PP06G_op">
+                                <option value="8">No cobró nada</option>
                         </select>
             </div>
         </div>
         <div class="form-row">
             <div class="form-group col-md-6">
                     <label for="PP06H">6h. ¿Es una actividad familiar? </label>
-                     <select class="form-control" name="PP06H"  id="PP06H"  >
+                     <select class="form-control" name="PP06H"  id="PP06H" >
                               <option value=" "> </option>
-                                <option value="1">Si</option>
-                                <option value="2" >No</option>
+                                <option value="1">1. Si</option>
+                                <option value="2">2. No</option>
                         </select>
                         <input type="text" class="form-control" name="PP06H_Esp" id="PP06H_Esp" maxlength="45">
 
             </div>
              <div class="form-group col-md-6">
                     <label for="PP06I">6i. De lo que produce o vende en su negocio/empresa/actividad, ¿retiró algo para consumo propio o de su hogar?</label>
-                      <select class="form-control" name="PP06I" id="PP06I" >
-                              <option value=" "> </option>
-                                <option value="1">Si</option>
-                                <option value="2" >No</option>
+                      <select class="form-control" name="PP06I" id="PP06I">
+                              <option value=" ">  </option>
+                                <option value="1">1. Si</option>
+                                <option value="2">2. No</option>
 
                         </select>
             </div>
@@ -93,10 +93,10 @@
             <div class="form-group col-md-6">
                     <label for="PP06J">6j. Por el mes de………………..,¿Recibió productos o mercaderías como pago por parte de un cliente? </label>
                      <small id="" class="form-text text-muted">(incluye créditos de trueque)</small>
-                     <select class="form-control" name="PP06J"  id="PP06J"  >
-                              <option value=" "> </option>
-                                <option value="1">Si</option>
-                                <option value="2" >No</option>
+                     <select class="form-control" name="PP06J"  id="PP06J" >
+                              <option value=" ">  </option>
+                                <option value="1">1. Si</option>
+                                <option value="2">2. No</option>
                         </select>
                         {{-- <input type="text" class="form-control" name="PP06H_Esp" id="PP06H_Esp" maxlength="45">   --}}
                    <br>
@@ -106,11 +106,11 @@
             </div>
              <div class="form-group col-md-6">
                     <label for="PP06K">6k. Lo ganado corresponde a su trabajo de…. </label>
-                      <select class="form-control" name="PP06K" id="PP06K" >
-                              <option value=" "> </option>
-                                <option value="1">…. Todo el mes trabajando todos los dás de la semana (por lo menos 5 días a la semana)</option>
-                                <option value="2" >… Todo el mes trabajando algunos días a la semana</option>
-                                 <option value="3">…. Menos de todo el mes</option>
+                      <select class="form-control" name="PP06K" id="PP06K">
+                              <option value=" ">  </option>
+                                <option value="1">1. …. Todo el mes trabajando todos los dás de la semana (por lo menos 5 días a la semana)</option>
+                                <option value="2">2. … Todo el mes trabajando algunos días a la semana</option>
+                                 <option value="3">3. …. Menos de todo el mes</option>
                         </select>
                         <div class="form-row">
                                 <div class="form-group col-md-4">
