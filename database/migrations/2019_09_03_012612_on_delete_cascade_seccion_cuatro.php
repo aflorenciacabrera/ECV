1194cc8ab72acfe36c10a350cfe9a1f895efc291
ivan sambrana <sambranaivan@gmail.com>
2019-09-03 08:48:17 -0300

    rename class name

--- database/migrations/2019_09_03_012612_on_delete_cascade_seccion_cuatro.php
+++ database/migrations/2019_09_03_012612_on_delete_cascade_seccion_cuatro.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class OnDeleteCascadeSeccionCuatro extends Migration
+class OnDeleteCascadeSeccionCincoVivienda extends Migration
 {
     /**
      * Run the migrations.
