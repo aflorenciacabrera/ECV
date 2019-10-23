6dcf099eca425a8e2a9494c7f2c2b85c1e18680b
Sambrana Ivan <sambranaivan@gmail.com>
2019-10-23 19:58:09 -0300

    cantidad de componenetes

--- app/Http/Controllers/reporteController.php
+++ app/Http/Controllers/reporteController.php
@@ -29,7 +29,6 @@ class reporteController extends Controller
         $viviendas = vivienda::all();
         $totales = $viviendas->count();
         $efectivas = vivienda::whereIn('CAUSAS',array('7','8','9'))->orWhereNull('CAUSAS')->get();
-
         $no_efectivas = vivienda::whereIn('CAUSAS', array('1', '2', '3', '4', '5', '6'))->get();
         $total_individuales = 0;
         $completos = 0;
@@ -60,11 +59,11 @@ class reporteController extends Controller
                 'efectivas' => $efectivas->count(),
                 'no_efectivas' => $no_efectivas->count(),
                 'deshabitada'=> $no_efectivas->where('CAUSAS','1')->count(),
-                                'demolida'=>$no_efectivas->where('CAUSAS','2')->count(),
-                                'fin_de_semana'=>$no_efectivas->where('CAUSAS','3')->count(),
-                                'construccion'=>$no_efectivas->where('CAUSAS','4')->count(),
-                                'establecimiento' => $no_efectivas->where('CAUSAS', '5')->count(),
-                                'variacion' => $no_efectivas->where('CAUSAS', '6')->count(),
+                'demolida'=>$no_efectivas->where('CAUSAS','2')->count(),
+                'fin_de_semana'=>$no_efectivas->where('CAUSAS','3')->count(),
+                'construccion'=>$no_efectivas->where('CAUSAS','4')->count(),
+                'establecimiento' => $no_efectivas->where('CAUSAS', '5')->count(),
+                'variacion' => $no_efectivas->where('CAUSAS', '6')->count(),
                 'sin_respuesta' => $efectivas->whereIn('CAUSAS',array('7','8','9'))->count(),
                 'con_respuesta' => $con_respuesta->count(),
                 'ausencias' => $efectivas->where('CAUSAS', '7')->count(),
